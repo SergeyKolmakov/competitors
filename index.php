efc350a8a872532600e89b84d32b3fe7aa93e426
Kolmakov Sergey <s-consul@mail.ru>
2020-10-19 17:29:24 +0400

    Fix index.php

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
 
-echo 'Test';
-$url = $_POST['url'];
+require_once '/lib/phpQuery.php';
+
+$url  = $_GET['url'];
+$html = file_get_contents($url);
+
+phpQuery::newDocument($html);
+$title = pq('title')->html();
+phpQuery::unloadDocuments();
+
+echo $title;
