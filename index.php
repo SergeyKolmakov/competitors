4d5f2a29905984504213d3e8ed4391efbda753db
Kolmakov Sergey <s-consul@mail.ru>
2020-10-22 15:47:07 +0400

    Fix final condition

--- index.php
+++ index.php
@@ -74,7 +74,7 @@ $answer['data']['price']       = $price;
 $answer['data']['minPrice']    = $minPrice;
 $answer['data']['maxPrice']    = $maxPrice;
 
-if ($isAvailable && !!$price) {
+if ($isAvailable && $price == 9999999) {
     thowError(505, 'Parser error. No price', $url);
 }
 
